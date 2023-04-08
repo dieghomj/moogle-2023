29291affc60c2f037bc2afc81f32008d80781d4a
dieghomj <diegomartj03@gmail.com>
2023-04-08 06:45:52 -0400

    Legibilty

--- MoogleEngine/Documents.cs
+++ MoogleEngine/Documents.cs
@@ -5,111 +5,166 @@ namespace MoogleEngine
 {
     public class Documents
     {
-        private Matrix Frequency;
-        private Matrix InverseD_Frequency;
+        private Matrix TF;
+        private Vector IDF;
         private string path; 
+        private string[] directory;
         private int documents;
         private int words;
-        private Dictionary<string,int> map;
+        private Dictionary<string,int> Vocabulary;
         
+        public static Matrix TFIDF;
 
         public Documents(string path){
 
             this.path = path;
             int documents = 0;
             
-            this.map = BuildMap();
+            this.Vocabulary = GetVocabulary();
+            this.directory = ReadDocuments(this.path);
 
-            foreach( string file in Directory.EnumerateFiles(path,"*.txt"))documents++;
+            foreach( string file in this.directory)documents++;
             this.documents = documents;
             
-            Frequency = new Matrix(this.documents+1,this.words+1);
-            InverseD_Frequency = new Matrix(this.words+1,1);
+            TF = new Matrix(this.documents+1,this.words+1);
+            IDF = new Vector(new double[words+1]);
 
-            this.CalculateTFIDF();
+            this.ComputeDocuments();
+
+            TFIDF = TF;
 
         }
 
-        public void CalculateTFIDF(){
+        public void ComputeDocuments(){
 
-            bool[] mk = new bool[this.words+1];
+            bool[] IsInDoc = new bool[this.words+1];
 
-            foreach( string file in Directory.EnumerateFiles(this.path,"*.txt")){
+            foreach( string file in directory){
 
-            int document = 0;
+                int document = 0;
 
-                foreach( var line in File.ReadLines(file,System.Text.Encoding.UTF8)){
+                string text = ReadText(this.path);
+                text = NormalForm(text);
 
-                    string s = NormalForm(line);
-                    string[] words = s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+                Vector tf = CalculateTF(text,Vocabulary);
 
-                    foreach(string word in words){
-                        if(!mk[map[word]]){
-                            this.InverseD_Frequency[map[word],0]++;
-                            mk[map[word]]=true;
-                        }
-                        this.Frequency[document,map[word]]++;
-                    }
-
+                for(int i = 0; i < this.words+1; i++){
+                    TF[document,i] = tf[i];
                 }
-                mk = new bool[this.words+1];
+
+                bool[] idf = WordsInDoc(text,Vocabulary);
+
+                for(int i = 0; i < words; i++){
+                    if(idf[i])IDF[i]++;
+                }
+            
                 document++;
+                
             }
 
-            Vector currentRow = new Vector(this.Frequency,0);            
-
-            for(int i = 0; i < this.Frequency.Size.rows; i++){
+            for(int i = 0; i < this.TF.Size.rows; i++){
                 
-                currentRow = new Vector(this.Frequency,i);
+                Vector currentRow = new Vector(this.TF,i);
 
-                for(int j = 0; j < this.Frequency.Size.columns; j++){
-
-                    this.InverseD_Frequency[j,0] = Math.Log((double)(this.documents)/(this.InverseD_Frequency[j,0]));
-                    
-                    this.Frequency[i,j] = 0.5f + 0.5f * (this.Frequency[i,j]/currentRow.MAX);
-
-                    this.Frequency[i,j] *=  this.InverseD_Frequency[j,0];
+                for(int j = 0; j < this.TF.Size.columns; j++){
+                    //Calcula IDF    
+                    this.IDF[j] = Math.Log((double)(this.documents)/(this.IDF[j]));
+                    //Normaliza el TF
+                    NormalizeTF(TF[i,j],currentRow.MAX);
+                    //Calcula TFID
+                    this.TF[i,j] *=  this.IDF[j];
                 }
             }
 
         }
 
-        private Dictionary<string,int> BuildMap(){
+        private Dictionary<string,int> GetVocabulary(){
 
-            Dictionary<string,int> map = new Dictionary<string, int>();
+            Dictionary<string,int> Vocabulary = new Dictionary<string, int>();
             int cnt = 0;
 
-            foreach( string file in Directory.EnumerateFiles(this.path,"*.txt")){
+            foreach( string file in directory){
                 
-                foreach( var line in File.ReadLines(file,System.Text.Encoding.UTF8)){
+                string text = ReadText(path);
+                
+                text = NormalForm(text);
 
-                    string s = NormalForm(line);
-                    string[] words = s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
-                    
-                    foreach(string word in words){
-                        if(map.ContainsKey(word))continue;
-                        else {
-                    
-                            map.Add(word,cnt++);
-                            
-                        }
+                string[] words = text.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+                
+                foreach(string word in words){
+                    if(Vocabulary.ContainsKey(word))continue;
+                    else {
+                
+                        Vocabulary.Add(word,cnt++);
+                        
                     }
                 }
+
             }
 
             this.words = cnt;
 
-            return map;
+            return Vocabulary;
 
         }
 
         public  (Matrix,Dictionary<string,int> ) GetDB{
-            get{ return (Frequency,map); }
+            get{ return (TF,Vocabulary); }
         }
 
-        private string NormalForm(string s){
+        public static Vector CalculateTF( string text, Dictionary<string,int> vocabulary){
+            
+            double[] A = new double[vocabulary.Count+1];
+            
+            text = NormalForm(text);
+
+            string[] words = text.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+
+            foreach(string word in words){
+                A[vocabulary[word]]++;
+            }
+
+            return new Vector(A);
+
+        }
+
+        public static double NormalizeTF(double tf, double max){
+           return 0.5f + 0.5f * (tf/max);
+        }
+
+        public static bool[] WordsInDoc(string text, Dictionary<string,int> vocabulary){
+                
+            bool[] IsInDoc = new bool[vocabulary.Count+1];
+            
+            text = NormalForm(text);
+            string[] words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach(string word in words){
+                
+                if(!IsInDoc[vocabulary[word]]){
+                
+                    IsInDoc[vocabulary[word]]=true;
+                
+                }
+
+            }
+
+            return IsInDoc;
+
+        }
+
+        public static Vector CalculateTFIDF(){return new Vector(0);}
+
+        private static string[] ReadDocuments(string path){
+            return Directory.GetDirectories(path,"*.txt");
+        }
+        private static string ReadText(string path){
+            return File.ReadAllText(path,System.Text.Encoding.UTF8);
+        }
+
+        private static string NormalForm(string s){
             s = s.ToLower();
-            s = s.Normalize( System.Text.NormalizationForm.FormKD);
+            s = Regex.Replace(s.Normalize( System.Text.NormalizationForm.FormD),@"[^a-zA-z0-9]+","");
             s = Regex.Replace(s,@"[.,;:?!`¨'¡-]?",string.Empty);
             return s;
         }
